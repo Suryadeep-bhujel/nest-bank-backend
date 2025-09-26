cf70ba0bb6db4345ef56952037f3581540ee7ba3
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-26 10:43:25 +0530

    Branch and CustomerAddress entity added in bank account service

--- src/bank-account/bank-account.module.ts
+++ src/bank-account/bank-account.module.ts
@@ -5,10 +5,12 @@ import { BankAccount, BankAccountCustomers } from '@src/bank-account/entities/ba
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from '@src/app.service';
 import { Customer } from '@src/customer/entities/customer.entity';
+import { Branch } from '@src/branch/entities/branch.entity';
+import { CustomerAddress } from '@src/customer-addresses/entities/customer-address.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BankAccount, Customer, BankAccountCustomers]),
+    TypeOrmModule.forFeature([BankAccount, Customer, BankAccountCustomers, Branch, CustomerAddress]),
   ],
   controllers: [BankAccountController],
   providers: [BankAccountService, AppService],
