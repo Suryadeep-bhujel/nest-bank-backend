b7e411651424b598dbdc2a2dccd05842fbd16725
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-26 08:53:07 +0530

    Customer Address table expanded by adding other related fields

--- src/customer-addresses/entities/customer-address.entity.ts
+++ src/customer-addresses/entities/customer-address.entity.ts
@@ -1,46 +1,69 @@
 
+import { AddressStatus } from "@bank-app-common/enum/SharedEnum";
 import { Customer } from "src/customer/entities/customer.entity";
 import { CommonEntity } from "src/shared/entities/CommonEntity";
+import { AddressType } from "src/shared/utils/SharedEnum";
 import { User } from "src/users/entities/user.entity";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity({ name: 'customer_addresses' })
 export class CustomerAddress extends CommonEntity {
 
-    @Column({ type: "enum", enum: ['HOME', "WORK", "OTHER", "NOT_SPECIFIED"], nullable: true , default: 'HOME' })
+    @Column({ type: "enum", enum: AddressType, nullable: true, default: AddressType.HOME })
     addressLabel?: string;
 
-    @Column({ length: 50 , nullable: true})
+    @Column({ length: 50, nullable: true })
     addressLine1?: string;
 
-    @Column({ length: 50, nullable: true})
+    @Column({ length: 50, nullable: true })
     addressLine2?: string;
 
-    @Column({ length: 50, nullable: true})
+    @Column({ length: 50, nullable: true })
     city?: string;
 
-    @Column({ length: 50, nullable: true})
+    @Column({ length: 50, nullable: true })
+    houseNo?: string;
+
+    @Column({ length: 50, nullable: true })
+    buildingName?: string;
+
+    @Column({ length: 50, nullable: true })
+    flatNo?: string;
+
+    @Column({ length: 50, nullable: true })
+    streetName?: string;
+
+    @Column({ length: 50, nullable: true })
+    streetNo?: string;
+
+    @Column({ length: 50, nullable: true })
+    landmark?: string;
+
+    @Column({ length: 50, nullable: true })
+    district?: string;
+
+    @Column({ length: 50, nullable: true })
     state?: string;
 
-    @Column({ length: 50, nullable: true})
+    @Column({ length: 50, nullable: true })
     country?: string;
 
-    @Column({ length: 50, nullable: true})
+    @Column({ length: 50, nullable: true })
     postalCode?: string;
 
-    @Column({ length: 50 , nullable: true})
+    @Column({ length: 50, nullable: true })
     phoneNumber?: string;
 
     @Column({ type: "bool", default: true })
     isDefault?: boolean;
 
-    @Column({ type: "enum", enum: ['ACTIVE', "INACTIVE", "DELETED", "NOT_SPECIFIED", "SUSPENDED", "ON_HOLD", "BLACK_LISTED"], default: 'ACTIVE' })
+    @Column({ type: "enum", enum: AddressStatus, default: AddressStatus.ACTIVE })
     status?: string;
 
     @Column({ type: "bool", default: false })
     isDeleted?: boolean;
 
-    @Column({ type: "date" , default: () => "CURRENT_TIMESTAMP" })
+    @Column({ type: "date", default: () => "CURRENT_TIMESTAMP" })
     createdAt?: Date;
 
     @Column({ type: "date", default: () => "CURRENT_TIMESTAMP" })
@@ -49,7 +72,7 @@ export class CustomerAddress extends CommonEntity {
     @Column({ type: "date", nullable: true })
     deletedAt?: Date;
 
-    @Column({ type: "bigint" , nullable: true })
+    @Column({ type: "bigint", nullable: true })
     addedById?: bigint;
 
     @ManyToOne(() => User, (staff) => staff.id, { nullable: true })
@@ -57,7 +80,7 @@ export class CustomerAddress extends CommonEntity {
     addedBy: User | null;
 
     @Column({ type: 'bigint', nullable: false })
-    customerId?: bigint;
+    customerId: bigint;
 
     @ManyToOne(() => Customer, (customer) => customer.id, { nullable: false })
     @JoinColumn({ name: 'customerId' })
