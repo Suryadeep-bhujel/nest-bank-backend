8027a8afb648b3d257818c38882461960a77510f
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-27 19:23:38 +0530

    customer and bank account seeder updated with new fields

--- src/seeds/bank-account.seeder.ts
+++ src/seeds/bank-account.seeder.ts
@@ -4,7 +4,8 @@ import { Customer } from '@src/customer/entities/customer.entity';
 import chunk from 'lodash/chunk';
 import { BaseSeeder } from '@src/seeds/base-seeder';
 import { BankAccount, BankAccountCustomers } from '@src/bank-account/entities/bank-account.entity';
-import { SharedStatus } from '@src/shared/utils/SharedEnum';
+import { BankAccountStatusType } from '@bank-app-common/enum/SharedEnum';
+import { Branch } from '@src/branch/entities/branch.entity';
 
 //npx ts-node src/seeds/customers.seeder.ts
 // run below command to seed data 
@@ -17,6 +18,9 @@ async function seed() {
     const customerRepo = AppDataSource.getRepository(Customer);
     const customers = await customerRepo.find({ select: { id: true } });
     console.log("Total customers found", customers.length)
+
+    const brancheRepo = AppDataSource.getRepository(Branch)
+    const branches = await brancheRepo.find({ select: { _oid: true, id: true } })
     const appService = new AppService();
     const accountRepo = AppDataSource.getRepository(BankAccount);
     const latestAccount = await accountRepo.find({ order: { id: 'DESC' }, take: 1 });
@@ -24,40 +28,44 @@ async function seed() {
     if (latestAccount.length > 0) {
         lastAccountNumber = latestAccount[0].accountNumber;
     }
-    
-    const accounts = Array.from({ length: 1500000 }).map(() => {
-        const account = new BankAccount();
-        lastAccountNumber = 'BA' + (parseInt(lastAccountNumber.replace(/\D/g, '')) + 1).toString().padStart(6, '0');
-        account._oid = appService.generateOid();
-        account.accountNumber = lastAccountNumber;
-        account.status = faker.helpers.arrayElement(Object.values(SharedStatus));
-        account.balance = 0;
-        account.accountType = faker.helpers.arrayElement(['savings', 'checking']);
-        account.currency = faker.helpers.arrayElement(['USD', 'EUR', 'GBP']);
-        return account;
-    });
-    const batches = chunk(accounts, 2000)
-    const customerBankArr: BankAccountCustomers[] = [];
-    for (const data of batches) {
-        const accounts = await accountRepo.save(data);
-        accounts.map((account) => {
-            const accountCustomer = new BankAccountCustomers();
-            accountCustomer.customerId = faker.helpers.arrayElement(customers)?.id;
-            accountCustomer.bankAccountId = account.id;
-            accountCustomer._oid = appService.generateOid();
-            accountCustomer.createdAt = faker.date.past();
-            accountCustomer.updatedAt = faker.date.recent();
-            customerBankArr.push(accountCustomer);
-        })
-        console.log(`✅ Seeded ${data.length} fake bank accounts`);
-    }
-    const customerBatches = chunk(customerBankArr, 2000);
-    for (const data of customerBatches) {
-        await AppDataSource.getRepository(BankAccountCustomers).save(data);
-        console.log(`✅ Seeded ${data.length} fake customer-bank account relationships`);
+    let totalAccounts = 0;
+    for (let index = 0; index < 70; index++) {
+        const accounts = Array.from({ length: 100000 }).map(() => {
+            const account = new BankAccount();
+            account.branchOid = faker.helpers.arrayElement(branches)?._oid;
+            lastAccountNumber = 'BA' + (parseInt(lastAccountNumber.replace(/\D/g, '')) + 1).toString().padStart(6, '0');
+            account._oid = appService.generateOid();
+            account.accountNumber = lastAccountNumber;
+            account.status = faker.helpers.arrayElement(Object.values(BankAccountStatusType));
+            // account.balance = "0.00";
+            account.accountType = faker.helpers.arrayElement(['savings', 'checking']);
+            account.currency = faker.helpers.arrayElement(['USD', 'EUR', 'GBP']);
+            return account;
+        });
+        totalAccounts += accounts.length
+        const batches = chunk(accounts, 2000)
+        const customerBankArr: BankAccountCustomers[] = [];
+        for (const data of batches) {
+            const accounts = await accountRepo.save(data);
+            accounts.map((account) => {
+                const accountCustomer = new BankAccountCustomers();
+                accountCustomer.customerId = faker.helpers.arrayElement(customers)?.id;
+                accountCustomer.bankAccountId = account.id;
+                accountCustomer._oid = appService.generateOid();
+                accountCustomer.createdAt = faker.date.past();
+                accountCustomer.updatedAt = faker.date.recent();
+                customerBankArr.push(accountCustomer);
+            })
+            console.log(`✅ Seeded ${data.length} fake bank accounts : ${index}`);
+        }
+        const customerBatches = chunk(customerBankArr, 2000);
+        for (const data of customerBatches) {
+            await AppDataSource.getRepository(BankAccountCustomers).save(data);
+            console.log(`✅ Seeded ${data.length} fake customer-bank account relationships`);
+        }
     }
 
-    console.log(`✅ Seeded ${accounts.length} fake customer-bank account relationships`);
+    console.log(`✅ Seeded ${totalAccounts} fake customer-bank account relationships`);
     await AppDataSource.destroy();
 }
 
--- src/seeds/customers.seeder.ts
+++ src/seeds/customers.seeder.ts
@@ -4,7 +4,7 @@ import { Customer } from '@src/customer/entities/customer.entity';
 import chunk from 'lodash/chunk';
 import { User } from '@src/users/entities/user.entity';
 import { BaseSeeder } from '@src/seeds/base-seeder';
-import { GenderType, MaritalStatus, Occupation, PersonTitle } from '@bank-app-common/enum/SharedEnum';
+import { ChooseOptions, DocumentType, GenderType, MaritalStatus, Occupation, PersonCaste, PersonTitle } from '@bank-app-common/enum/SharedEnum';
 
 //npx ts-node src/seeds/customers.seeder.ts
 // run below command to seed data 
@@ -16,8 +16,25 @@ async function seed() {
     await AppDataSource.initialize();
     const branchRepo = AppDataSource.getRepository(Customer);
     const appService = new AppService();
-    const customers = Array.from({ length: 500000 }).map(() => {
+    const existing = (await branchRepo.find({ select: { officialIdNo: true } })).map(item => item.officialIdNo)
+    function checkDuplicate() {
+        let unique = false;
+        let num = faker.helpers.rangeToNumber({ min: 1000000000, max: 9000000000 });
+        do {
+            if (existing.indexOf(`ID${num}`) !== -1) {
+                num = faker.helpers.rangeToNumber({ min: 1000000000, max: 9000000000 });
+            } else {
+                unique = true;
+            }
+            // console.log("uniqueunique", unique, existing.indexOf(`ID${num}`))
+        } while (!unique)
+        existing.push(`ID${num}`)
+        return num;
+    }
+    let i=0;
+    const customers = Array.from({ length: 500000 }).map((row: number) => {
         const customer = new Customer();
+        let num = checkDuplicate()
         customer._oid = appService.generateOid();
         customer.dateOfBirth = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
         customer.email = faker.internet.email();
@@ -31,16 +48,28 @@ async function seed() {
         customer.countryOfResidence = "IN"
         customer.occupation = faker.helpers.arrayElement(Object.values(Occupation))
         customer.maritalStatus = faker.helpers.arrayElement(Object.values(MaritalStatus))
-
-
-        customer.phoneNumber = faker.phone.number({style: 'international'})
+        customer.officialIdNo = `ID${num}`
+        customer.category = faker.helpers.arrayElement(Object.values(PersonCaste))
+        customer.telephone = faker.phone.number({ style: 'international' })
+        customer.phoneNumber = faker.phone.number({ style: 'international' })
         customer.addedByStaffId = BigInt(1)
+        customer.guardianName = faker.person.fullName()
+        customer.introducerName = faker.person.fullName()
+        customer.kycDocumentProvided = faker.helpers.arrayElement(Object.values(ChooseOptions))
+        customer.kycDocumentType = faker.helpers.arrayElement(Object.values(DocumentType))
+        // customer.mobileNoCountryCode = faker.phone.number({style: 'national'})
+        customer.panNumber = `PAN${num}A`
+        customer.requestedDebitCard = faker.helpers.arrayElement(Object.values(ChooseOptions))
+        customer.requestedAddOn = faker.helpers.arrayElement(Object.values(ChooseOptions))
+        customer.perAnnumIncome = 2000
+        console.log(`Data mapping in progress : ${i}`)
+        i++
         return customer;
     });
     const batches = chunk(customers, 2000)
-    for (const data of batches) {
-        await branchRepo.save(data);
-        console.log(`✅ Seeded ${data.length} fake customers`);
+    for (const key in batches) {
+        await branchRepo.save(batches[key]);
+        console.log(`✅ Seeded ${batches[key].length} fake customers ${key}`);
     }
     console.log(`✅ Seeded ${customers.length} fake customers`);
     await AppDataSource.destroy();
